de54526cbede7e498f5ea372f2a5ef356d5dee1e
ih par <ismail@cs.deu.edu.tr>
2021-10-13 14:27:04 +0300

    trained all the vectors

--- main.py
+++ main.py
@@ -9,26 +9,36 @@ def main():
     pd = PitchDictionary("dataset_objects/pitches_dict.txt")
 
     vocab_size = pd.get_vocabulary_size()
-    window_size = 4
-    num_ns = 5
-    embedding_dim = 32
+    window_size = 4  # 2, 4
+    num_ns = 10  # 2, 5, 10
     embedding_layer_name = "n2v_embedding"
 
     db = DatasetBuilder("dataset_objects/full_corpus")
     dataset = db.build_word_to_vec_dataset(
-        vocab_size, window_size, num_ns, skip_amount=10)
+        vocab_size, window_size, num_ns, skip_amount=1)
 
+    embedding_dim = 16
     n2v = build_note_to_vec_model(
         vocab_size, embedding_dim, num_ns, embedding_layer_name)
 
-    n2v.fit(dataset, epochs=20)
+    epochs = 20
+    n2v.fit(dataset, epochs=epochs)
 
     vocab = pd.get_vocabulary()
     weights = n2v.get_layer(embedding_layer_name).get_weights()[0]
-    vectors_file = io.open('vectors.tsv', 'w', encoding='utf-8')
-    metadata_file = io.open('metadata.tsv', 'w', encoding='utf-8')
+
+    file_suffix = "ws" + str(window_size) + "_ns" + str(num_ns) + \
+        "_ed" + str(embedding_dim) + "_ep" + str(epochs)
+
+    vactors_file_name = "vectors_" + file_suffix + ".tsv"
+    metadata_file_name = "metadata_" + file_suffix + ".tsv"
+
+    vectors_file = io.open(vactors_file_name, 'w', encoding='utf-8')
+    metadata_file = io.open(metadata_file_name, 'w', encoding='utf-8')
 
     for index, word in enumerate(vocab):
+        if index in [0, 1]:  # <unk> and es
+            continue
         vec = weights[index]
         vectors_file.write('\t'.join([str(x) for x in vec]) + "\n")
         metadata_file.write(word + "\n")
