8e8908d34cd11343ace5fb9d9380f3e2c3c3a721
ih par <ismail@cs.deu.edu.tr>
2021-10-15 12:59:51 +0300

    a is to b

--- dummy.py
+++ dummy.py
@@ -27,26 +27,32 @@ def clean_below_500():
 
 
 def cosine_similarity(v1, v2):
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    denominator = np.linalg.norm(v1) * np.linalg.norm(v2)
+    if np.isclose(denominator, 0, atol=1e-32):
+        return 0
+    return np.dot(v1, v2) / denominator
 
 
-def find_cos_similar(note_name):
-    dir_path = "dataset_objects/dropped_500_vectors/"
-    cleaned_meta_path = dir_path + "clean_metadata_ws4_ns10_ed16_ep20.tsv"
-    cleaned_vector_path = dir_path + "clean_vectors_ws4_ns10_ed16_ep20.tsv"
+def get_vectors(dir_path, meta_file, vector_file):
+    meta_path = dir_path + meta_file
+    vector_path = dir_path + vector_file
 
     meta_lines, vector_lines = None, None
     vector_dict = {}
-    with Path(cleaned_meta_path).open("r") as meta_file, Path(cleaned_vector_path).open("r") as vector_file:
+    with Path(meta_path).open("r") as meta_file, Path(vector_path).open("r") as vector_file:
         meta_lines = meta_file.read().splitlines()
         vector_lines = vector_file.read().splitlines()
         for ml, vl in zip(meta_lines, vector_lines):
             vector_dict[ml] = np.array([float(v) for v in vl.split("\t")])
+    return vector_dict
 
-    # print(note_name, vector_dict[note_name])
-    max_sim = -1
+
+def find_cos_similar(note_name):
+    vector_dict = get_vectors("dataset_objects/dropped_500_vectors/",
+                              "clean_metadata_ws4_ns10_ed16_ep20.tsv",
+                              "clean_vectors_ws4_ns10_ed16_ep20.tsv")
+
     target_vec = vector_dict[note_name]
-    closest_pitch = [None, None]
     similarities = []
     for n, v in vector_dict.items():
         if n == note_name:
@@ -58,10 +64,37 @@ def find_cos_similar(note_name):
     return sorted(similarities, key=lambda tup: tup[1], reverse=True)
 
 
+def a_is_to_b_as_c_is_to(notes_a_b, note_c):
+    vector_dict = get_vectors("dataset_objects/dropped_500_vectors/",
+                              "clean_metadata_ws4_ns10_ed16_ep20.tsv",
+                              "clean_vectors_ws4_ns10_ed16_ep20.tsv")
+    n_a, n_b = notes_a_b
+    if n_a not in vector_dict or n_b not in vector_dict or note_c not in vector_dict:
+        return None
+
+    b_minus_a = vector_dict[n_b] - vector_dict[n_a]
+    vec_c = vector_dict[note_c]
+    max_sim = -100
+    note_d = None
+    for n, v in vector_dict.items():
+        if n == note_c:
+            continue
+
+        cos_sim = cosine_similarity(b_minus_a, (v - vec_c))
+        if cos_sim > max_sim:
+            max_sim = cos_sim
+            note_d = (n, v)
+
+    return note_d
+
+
 def main():
-    similarities = find_cos_similar("do5")[:10]
+    similarities = find_cos_similar("la4")[:5]
     for s in similarities:
         print(s[0], s[1])
+
+    n_d = a_is_to_b_as_c_is_to(("la4", "re5"), "re4")
+    print(n_d)
 
 
 if __name__ == "__main__":
